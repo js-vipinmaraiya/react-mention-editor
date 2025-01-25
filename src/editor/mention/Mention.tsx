17010d9f3f7d58eef7b196daa1bc4652cad85eda
js-vipinmaraiya <js.vipinmaraiya@gmail.com>
2025-01-25 22:56:15 +0530

    Update Mention.tsx

--- src/editor/mention/Mention.tsx
+++ src/editor/mention/Mention.tsx
@@ -1,23 +1,31 @@
+
+// Import the necessary type definition for the Mention component's props
 import { EntryComponentProps } from "@draft-js-plugins/mention/lib/MentionSuggestions/Entry/Entry";
 
+// Define the Mention component
 const Mention = (props: EntryComponentProps): JSX.Element => {
+  // eslint-disable-line @typescript-eslint/no-unused-vars
   const {
-    mention,
-    theme,
-    selectMention, // eslint-disable-line @typescript-eslint/no-unused-vars
-    searchValue, // eslint-disable-line @typescript-eslint/no-unused-vars
-    isFocused, // eslint-disable-line @typescript-eslint/no-unused-vars
-    ...parentProps
+    mention, // The mention object containing details like name and email
+    theme, // Theme object used to apply custom styles
+    selectMention, //  Function to handle mention selection (not used here)
+    searchValue, // The current search value typed by the user (not used here)
+    isFocused, // Whether this mention is currently focused in the suggestions list (not used here)
+    ...parentProps // Any additional props passed to the Mention component
   } = props;
 
   return (
     <div {...parentProps}>
-       <div className={theme?.mentionSuggestionsEntryText}>
+      {/* Wrapper for mention entry */}
+      <div className={theme?.mentionSuggestionsEntryText}>
+        {/* Display the mention's name */}
         <span>{mention.name}</span>
+        {/* Display the mention's email with a separate style */}
         <span className={theme?.email}>{mention.email}</span>
-       </div>
+      </div>
     </div>
   );
 };
 
+// Export the Mention component as default
 export default Mention;
