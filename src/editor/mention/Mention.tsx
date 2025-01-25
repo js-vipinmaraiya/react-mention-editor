02881df95452d5b0c59e97004138ca1931c03892
js-vipinmaraiya <js.vipinmaraiya@gmail.com>
2025-01-25 23:02:11 +0530

    Update Mention.tsx

--- src/editor/mention/Mention.tsx
+++ src/editor/mention/Mention.tsx
@@ -1,21 +1,23 @@
-
 // Import the necessary type definition for the Mention component's props
 import { EntryComponentProps } from "@draft-js-plugins/mention/lib/MentionSuggestions/Entry/Entry";
 
 // Define the Mention component
 const Mention = (props: EntryComponentProps): JSX.Element => {
-  // eslint-disable-line @typescript-eslint/no-unused-vars
   const {
     mention, // The mention object containing details like name and email
     theme, // Theme object used to apply custom styles
-    selectMention, //  Function to handle mention selection (not used here)
+    selectMention, // Function to handle mention selection (not used here)
     searchValue, // The current search value typed by the user (not used here)
     isFocused, // Whether this mention is currently focused in the suggestions list (not used here)
     ...parentProps // Any additional props passed to the Mention component
   } = props;
 
+  // Exclude selectMention, searchValue, and isFocused from parentProps
+  const { selectMention: _ignored1, searchValue: _ignored2, isFocused: _ignored3, ...filteredProps } =
+    parentProps;
+
   return (
-    <div {...parentProps}>
+    <div {...filteredProps}>
       {/* Wrapper for mention entry */}
       <div className={theme?.mentionSuggestionsEntryText}>
         {/* Display the mention's name */}
